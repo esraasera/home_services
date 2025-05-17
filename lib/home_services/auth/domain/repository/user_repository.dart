c96ae425681192f99ad104be9e38883ab1057d82
esraasera <esraam9756@gmail.com>
2025-05-17 18:02:38 +0300

    add auth repo

---
+++ lib/home_services/auth/domain/repository/user_repository.dart
@@ -0,0 +1,6 @@
+import 'package:home_services_app/home_services/auth/domain/entities/user_entity.dart';
+
+abstract class AuthRepository {
+
+  Future<void>register(UserEntity user , String password);
+}
