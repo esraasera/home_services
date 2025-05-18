92cd7c6e4147942cc680995b0b7f0bf0ffd5c243
esraasera <esraam9756@gmail.com>
2025-05-18 14:04:59 +0300

    add user model screen

---
+++ lib/home_services/auth/data/models/user_model.dart
@@ -0,0 +1,10 @@
+import 'package:home_services_app/home_services/auth/domain/entities/user_entity.dart';
+
+class UserModel extends UserEntity{
+  UserModel({
+    required super.email,
+    required super.name,
+   });
+
+
+}
