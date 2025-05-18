f20dbedcd5b67091a80f5a591742345cf8780d3c
esraasera <esraam9756@gmail.com>
2025-05-18 15:00:29 +0300

    add some changes

--- lib/home_services/auth/data/datasource/auth_remote_datasource.dart
+++ lib/home_services/auth/data/datasource/auth_remote_datasource.dart
@@ -1,5 +1,24 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:home_services_app/home_services/auth/data/models/user_model.dart';
 
 abstract class AuthRemoteDataSource {
   Future<void> registerUser(UserModel user , String password);
 }
+
+class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
+  final FirebaseAuth firebaseAuth;
+  final FirebaseFirestore firestore;
+
+  AuthRemoteDataSourceImpl({
+    required this.firebaseAuth,
+    required this.firestore,
+});
+  @override
+  Future<void> registerUser(UserModel user, String password)async {
+
+    await firebaseAuth.createUserWithEmailAndPassword(email: user.email, password: password);
+
+  }
+
+} 
