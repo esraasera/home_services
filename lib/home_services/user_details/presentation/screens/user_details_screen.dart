a64a54196112b660aa5bdbbaba90388ffba7ba57
esraasera <esraam9756@gmail.com>
2025-05-20 20:08:45 +0300

    add some changes

---
+++ lib/home_services/user_details/presentation/screens/user_details_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class UserDetailsScreen extends StatelessWidget{
+  const UserDetailsScreen({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("User Details Screen"),
+    );
+  }
+
+}
