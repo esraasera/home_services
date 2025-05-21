022d6f58b0428c03858004b74830403ca2d8fcc3
esraasera <esraam9756@gmail.com>
2025-05-21 19:29:55 +0300

    finish auth data repo

--- lib/home_services/auth/data/repository/auth_repository.dart
+++ lib/home_services/auth/data/repository/auth_repository.dart
@@ -1,14 +1,17 @@
+import 'package:home_services_app/home_services/auth/data/datasource/auth_remote_datasource.dart';
+import 'package:home_services_app/home_services/auth/data/models/user_model.dart';
 import 'package:home_services_app/home_services/auth/domain/entities/user_entity.dart';
 import 'package:home_services_app/home_services/auth/domain/repository/base_auth_repository.dart';
 
 class AuthRepositoryImpl implements AuthRepository{
+  AuthRemoteDataSource remoteDataSource ;
 
-
+  AuthRepositoryImpl(this.remoteDataSource);
 
   @override
-  Future<void> registerUser(UserEntity user, String password) {
-    // TODO: implement registerUser
-    throw UnimplementedError();
+  Future<void> registerUser(UserEntity user, String password) async {
+    final model = UserModel(name: user.name, email: user.email);
+    await remoteDataSource .registerUser(model, password);
   }
 
 }
