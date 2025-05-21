203dab6aff2524a2a39e3e4f578d14bc306bec36
esraasera <esraam9756@gmail.com>
2025-05-21 20:03:24 +0300

    add register cubit

---
+++ lib/home_services/auth/presentation/controller/register_cubit.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:home_services_app/home_services/auth/domain/entities/user_entity.dart';
+import 'package:home_services_app/home_services/auth/domain/usecases/register_useCase.dart';
+import 'package:home_services_app/home_services/auth/presentation/controller/register_states.dart';
+
+class RegisterCubit extends Cubit <RegisterStates>{
+  RegisterCubit(this.registerUseCase) : super(RegisterInitial());
+
+  static RegisterCubit get(context) => BlocProvider.of(context);
+  final RegisterUseCase registerUseCase;
+
+  Future<void> registerUser(String email , String name ,String password) async{
+    emit(RegisterLoading());
+    try{
+      final user = UserEntity(email: email, name: name);
+      await registerUseCase (user , password);
+      emit(RegisterSuccess());
+    }catch (e) {
+      emit(RegisterFailure(e.toString()));
+    }
+  }
+}
--- lib/home_services/auth/presentation/controller/register_states.dart
+++ lib/home_services/auth/presentation/controller/register_states.dart
@@ -1,9 +1,9 @@
-abstract class RegisterState {}
+abstract class RegisterStates {}
 
-class RegisterInitial extends RegisterState {}
-class RegisterLoading extends RegisterState {}
-class RegisterSuccess extends RegisterState {}
-class RegisterFailure extends RegisterState {
+class RegisterInitial extends RegisterStates {}
+class RegisterLoading extends RegisterStates {}
+class RegisterSuccess extends RegisterStates {}
+class RegisterFailure extends RegisterStates {
   final String message;
   RegisterFailure(this.message);
 }
