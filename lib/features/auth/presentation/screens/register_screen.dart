bf3de0b92287548a117ebe790f0f1e30bafaec68
esraasera <esraam9756@gmail.com>
2025-05-22 18:56:04 +0300

    finish register cubit

--- lib/features/auth/presentation/screens/register_screen.dart
+++ lib/features/auth/presentation/screens/register_screen.dart
@@ -1,127 +1,153 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:home_services_app/core/routing/app_routes.dart';
 import 'package:home_services_app/core/theme/app_colors.dart';
 import 'package:home_services_app/core/theme/styles_manager.dart';
 import 'package:home_services_app/core/utils/app_string.dart';
 import 'package:home_services_app/core/values/app_values.dart';
+import 'package:home_services_app/features/auth/data/datasource/auth_remote_datasource.dart';
+import 'package:home_services_app/features/auth/data/repository/auth_repository.dart';
+import 'package:home_services_app/features/auth/domain/entities/user_entity.dart';
+import 'package:home_services_app/features/auth/domain/usecases/register_useCase.dart';
+import 'package:home_services_app/features/auth/presentation/controller/register_cubit.dart';
+import 'package:home_services_app/features/auth/presentation/controller/register_states.dart';
 
 class RegisterScreen extends StatelessWidget{
   const RegisterScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final TextEditingController userNameController = TextEditingController();
+    final TextEditingController nameController = TextEditingController();
     final TextEditingController emailController = TextEditingController();
-    final TextEditingController userPasswordController = TextEditingController();
+    final TextEditingController passwordController = TextEditingController();
+    var cubit = RegisterCubit.get(context);
+    final user = UserEntity(email: emailController.text, name: nameController.text);
     final formKey = GlobalKey<FormState>();
-    return Scaffold(
+    return BlocProvider(
+      create: (BuildContext context) =>RegisterCubit(
+          RegisterUseCase(
+              AuthRepositoryImpl(
+              AuthRemoteDataSourceImpl(
+              firebaseAuth: FirebaseAuth.instance,
+                firestore: FirebaseFirestore.instance,
+              )
+              )
+          )
+          ),
+      child: BlocConsumer<RegisterCubit,RegisterStates>(
+      listener:(BuildContext context,RegisterStates state ){},
+      builder:(BuildContext context , RegisterStates state){
+        return Scaffold(
         backgroundColor: AppColors.white,
         body: Center(
-          child: SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.all(AppPadding.p20),
-                child: Container(
-                  margin: EdgeInsets.symmetric(vertical: AppMargin.m40),
-                  child: Form(
-                    key:formKey,
-                    child: Column(
-                        children: [
-                          Text(
-                            AppStrings.register,
-                            style: getBoldStyle(color: AppColors.black,fontSize: AppSize.s35),
-                          ),
-                          SizedBox(
-                            height: AppSize.s30,
-                          ),
-                          TextFormField(
-                            keyboardType: TextInputType.emailAddress,
-                            controller: emailController,
-                            decoration: InputDecoration(
-                              hintText: AppStrings.email,
-                              labelText: AppStrings.email,
-                            ),
-                            validator: (value) {
-                              if (value == null || value.isEmpty) {
-                                return AppStrings.emailError;
-                              }
-                              return null;
-                            },),
-                          SizedBox(
-                            height: AppSize.s20,
-                          ),
-                          TextFormField(
-                            keyboardType: TextInputType.emailAddress,
-                            controller: userNameController,
-                            decoration: InputDecoration(
-                              hintText: AppStrings.username,
-                              labelText: AppStrings.username,
-                            ),
-                            validator: (value) {
-                              if (value == null || value.isEmpty) {
-                                return AppStrings.usernameError;
-                              }
-                              return null;
-                            },),
-                          SizedBox(
-                            height: AppSize.s20,
-                          ),
-                          TextFormField(
-                            keyboardType: TextInputType.visiblePassword,
-                            controller: userPasswordController,
-                            obscureText: true,
-                            decoration: InputDecoration(
-                              hintText: AppStrings.password,
-                              labelText: AppStrings.password,
-                            ),
-                            validator: (value) {
-                              if (value == null || value.isEmpty) {
-                                return AppStrings.passwordError;
-                              }
-                              return null;
-                            },),
-                          SizedBox(
-                            height: AppSize.s40,
-                          ),
-                          ElevatedButton(
-                            onPressed: () {
-                              if (formKey.currentState!.validate()) {
-                              }
-                            },
-                            child: Text(
-                              AppStrings.register,
-                            ),
-                          ),
-                          SizedBox(
-                            height: AppSize.s28,
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                AppStrings.alreadyUser,
-                                style: getBoldStyle(color: AppColors.black,fontSize:AppSize.s16),
-                              ),
-                              SizedBox(
-                                width: AppSize.s1_5,
-                              ),
-                              GestureDetector(
-                                child: Text(
-                                  AppStrings.login,
-                                  style: getBoldStyle(color: AppColors.primary,fontSize: AppSize.s16),
-                                ),
-                                onTap:(){
-                                  Navigator.of(context).pushReplacementNamed(Routes.loginRoute);
-                                } ,
-                              ),
-                            ],
-                          ),
-                        ]),
-                  ),
-                ),
-              )
-          ),
-        )
-    );
-  }
-
+        child: SingleChildScrollView(
+        child: Padding(
+        padding: const EdgeInsets.all(AppPadding.p20),
+        child: Container(
+        margin: EdgeInsets.symmetric(vertical: AppMargin.m40),
+        child: Form(
+        key:formKey,
+        child: Column(
+        children: [
+        Text(
+        AppStrings.register,
+        style: getBoldStyle(color: AppColors.black,fontSize: AppSize.s35),
+        ),
+        SizedBox(
+        height: AppSize.s30,
+        ),
+      TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      controller: emailController,
+      decoration: InputDecoration(
+      hintText: AppStrings.email,
+      labelText: AppStrings.email,
+      ),
+      validator: (value) {
+      if (value == null || value.isEmpty) {
+      return AppStrings.emailError;
+      }
+      return null;
+      },),
+      SizedBox(
+      height: AppSize.s20,
+      ),
+      TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      controller: nameController,
+      decoration: InputDecoration(
+      hintText: AppStrings.username,
+      labelText: AppStrings.username,
+      ),
+      validator: (value) {
+      if (value == null || value.isEmpty) {
+      return AppStrings.usernameError;
+      }
+      return null;
+      },),
+      SizedBox(
+      height: AppSize.s20,
+      ),
+      TextFormField(
+      keyboardType: TextInputType.visiblePassword,
+      controller: passwordController,
+      obscureText: true,
+      decoration: InputDecoration(
+      hintText: AppStrings.password,
+      labelText: AppStrings.password,
+      ),
+      validator: (value) {
+      if (value == null || value.isEmpty) {
+      return AppStrings.passwordError;
+      }
+      return null;
+      },),
+      SizedBox(
+      height: AppSize.s40,
+      ),
+      ElevatedButton(
+      onPressed: () {
+      if (formKey.currentState!.validate()) {
+        cubit.registerUseCase(user,passwordController.text);
+      }
+      },
+      child: Text(
+      AppStrings.register,
+      ),
+      ),
+      SizedBox(
+      height: AppSize.s28,
+      ),
+      Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+      Text(
+      AppStrings.alreadyUser,
+      style: getBoldStyle(color: AppColors.black,fontSize:AppSize.s16),
+      ),
+      SizedBox(
+      width: AppSize.s1_5,
+      ),
+      GestureDetector(
+      child: Text(
+      AppStrings.login,
+      style: getBoldStyle(color: AppColors.primary,fontSize: AppSize.s16),
+      ),
+      onTap:(){
+      Navigator.of(context).pushReplacementNamed(Routes.loginRoute);
+      } ,
+      ),
+      ],
+      ),
+      ]),
+      ),
+      ),
+      )
+      ),
+      )
+      );
+      },
+    ));}
 }
