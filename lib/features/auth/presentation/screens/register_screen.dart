bfe687e0a0190eb57ece36faf7aa8b6ae7fb6c2d
esraasera <esraam9756@gmail.com>
2025-05-25 22:11:32 +0300

    finish login ui

--- lib/features/auth/presentation/screens/register_screen.dart
+++ lib/features/auth/presentation/screens/register_screen.dart
@@ -94,7 +94,7 @@ class RegisterScreen extends StatelessWidget{
       return null;
       },),
       SizedBox(
-      height: AppSize.s28,
+      height: AppSize.s20,
       ),
       TextFormField(
       keyboardType: TextInputType.visiblePassword,
