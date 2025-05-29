96f44aa6369aa52384fc5ed04f178dd722aeb439
esraasera <esraam9756@gmail.com>
2025-05-30 00:04:32 +0300

    add forgot password data source

--- lib/features/auth/presentation/screens/forgot_password.dart
+++ lib/features/auth/presentation/screens/forgot_password.dart
@@ -54,7 +54,7 @@ class ForgotPassword extends StatelessWidget{
               body: Center(
                 child: SingleChildScrollView(
                     child: Padding(
-                      padding: const EdgeInsets.all(AppPadding.p8),
+                      padding: const EdgeInsets.all(AppPadding.p12),
                       child: Container(
                         margin: EdgeInsets.symmetric(vertical: AppMargin.m40),
                         child: Form(
