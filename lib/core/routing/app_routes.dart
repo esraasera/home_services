ec6296790d011032c4f14c0f37b996cbd14b1555
esraasera <esraam9756@gmail.com>
2025-05-31 20:19:00 +0300

    edit app routes

--- lib/core/routing/app_routes.dart
+++ lib/core/routing/app_routes.dart
@@ -1,14 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:home_services_app/core/utils/app_string.dart';
-import 'package:home_services_app/features/auth/presentation/screens/forgot_password.dart';
+import 'package:home_services_app/features/auth/presentation/screens/forgot_password_screen.dart';
 import 'package:home_services_app/features/auth/presentation/screens/login_screen.dart';
 import 'package:home_services_app/features/auth/presentation/screens/register_screen.dart';
 import 'package:home_services_app/features/chat/presentation/screens/chat_screen.dart';
-import 'package:home_services_app/features/location/presentation/screens/location_screen.dart';
 import 'package:home_services_app/features/rating/presentation/screens/rating_screen.dart';
-import 'package:home_services_app/features/services/presentation/screens/services_screen.dart';
+import 'package:home_services_app/features/service_request/presentation/screens/stepper_screen.dart';
 import 'package:home_services_app/features/splash/presentation/screens/splash_screen.dart';
-import 'package:home_services_app/features/user_details/presentation/screens/user_details_screen.dart';
 
 class Routes{
   static const String splashRoute = "/";
@@ -16,10 +14,6 @@ class Routes{
   static const String registerRoute = "/register";
   static const String forgotPasswordRoute = "/forgotPassword";
   static const String stepperRoute = "/stepper";
-  static const String servicesRoute = "/services";
-  static const String userDetailsScreenRoute = "/userDetails";
-  static const String locationRoute = "/location";
-  static const String confirmationRoute = "/confirmation";
   static const String ratingRoute = "/rating";
   static const String chatRoute = "/chat";
 }
@@ -38,12 +32,6 @@ static Route <dynamic> getRoute(RouteSettings settings){
       return MaterialPageRoute(builder: (_) => ForgotPassword());
     case Routes.stepperRoute:
       return MaterialPageRoute(builder: (_) => StepperScreen());
-    case Routes.servicesRoute:
-      return MaterialPageRoute(builder: (_) => ServicesScreen());
-    case Routes.userDetailsScreenRoute:
-      return MaterialPageRoute(builder: (_) => UserDetailsScreen());
-    case Routes.locationRoute:
-      return MaterialPageRoute(builder: (_) => LocationScreen());
     case Routes.chatRoute:
       return MaterialPageRoute(builder: (_) => ChatScreen());
     case Routes.ratingRoute:
---
+++ lib/features/service_request/presentation/screens/stepper_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+class StepperScreen extends StatelessWidget{
+  const StepperScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
