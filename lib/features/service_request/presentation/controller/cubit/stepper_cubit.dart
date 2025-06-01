1c9bfe44d06cc81c4485db0bc871c132937acd59
esraasera <esraam9756@gmail.com>
2025-06-01 18:40:38 +0300

    changes in stepper states

--- lib/features/service_request/presentation/controller/cubit/stepper_cubit.dart
+++ lib/features/service_request/presentation/controller/cubit/stepper_cubit.dart
@@ -1,25 +1,27 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter/material.dart';
 import 'package:home_services_app/features/service_request/presentation/controller/states/stepper_states.dart';
 import 'package:home_services_app/features/service_request/presentation/screens/location_screen.dart';
 import 'package:home_services_app/features/service_request/presentation/screens/payment_screen.dart';
 import 'package:home_services_app/features/service_request/presentation/screens/services_screen.dart';
 import 'package:home_services_app/features/service_request/presentation/screens/user_info_screen.dart';
 
+
 class StepperCubit extends Cubit<StepperState> {
   StepperCubit() : super(StepperInitial());
+
   static StepperCubit get(context) => BlocProvider.of(context);
 
   int currentStep = 0;
 
-  void nextStepperStep() {
+  void nextStep() {
     if (currentStep < steps.length - 1) {
       currentStep++;
       emit(StepperChanged());
     }
   }
 
-  void previousStepperStep() {
+  void previousStep() {
     if (currentStep > 0) {
       currentStep--;
       emit(StepperChanged());
@@ -33,31 +35,17 @@ class StepperCubit extends Cubit<StepperState> {
     }
   }
 
-  List<Step> get steps => [
-    Step(
-      title: const Text('Service'),
-      content: SizedBox(height: 300, child: const ServicesScreen()),
-      isActive: currentStep >= 0,
-      state: currentStep > 0 ? StepState.complete : StepState.indexed,
-    ),
-    Step(
-      title: const Text('Details'),
-      content: const UserInfoScreen(),
-      isActive: currentStep >= 1,
-      state: currentStep > 1 ? StepState.complete : StepState.indexed,
-    ),
-    Step(
-      title: const Text('Address'),
-      content: const LocationScreen(),
-      isActive: currentStep >= 2,
-      state: currentStep > 2 ? StepState.complete : StepState.indexed,
-    ),
-    Step(
-      title: const Text('Payment'),
-      content: const PaymentScreen(),
-      isActive: currentStep >= 3,
-      state:
-      currentStep == 3 ? StepState.editing : StepState.indexed,
-    ),
+  List<Widget> get steps => [
+    const ServicesScreen(),
+    const UserInfoScreen(),
+    const LocationScreen(),
+    const PaymentScreen(),
+  ];
+
+  List<String> get stepTitles => [
+    'Service',
+    'Details',
+    'Address',
+    'Payment',
   ];
 }
--- lib/features/service_request/presentation/controller/states/stepper_states.dart
+++ lib/features/service_request/presentation/controller/states/stepper_states.dart
@@ -1,6 +1,5 @@
 abstract class StepperState {}
+
 class StepperInitial extends StepperState {}
+
 class StepperChanged extends StepperState {}
-
-
-
