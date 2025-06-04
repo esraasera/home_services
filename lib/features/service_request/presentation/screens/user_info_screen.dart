a58034c48d52983875f564f28c67397b3fabb83b
esraasera <esraam9756@gmail.com>
2025-06-04 20:55:53 +0300

    add user info screen

--- lib/features/service_request/presentation/screens/user_info_screen.dart
+++ lib/features/service_request/presentation/screens/user_info_screen.dart
@@ -1,11 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:home_services_app/core/theme/app_colors.dart';
+import 'package:home_services_app/core/utils/app_strings.dart';
+import 'package:home_services_app/core/values/app_values.dart';
 
 class UserInfoScreen extends StatelessWidget {
   const UserInfoScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('User Info Content'));
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        TextFormField(),
+        SizedBox(
+          height: AppSize.s30,
+        ),
+        TextFormField(),
+        SizedBox(
+          height: AppSize.s30,
+        ),
+        Row(
+          children: [
+            ElevatedButton(
+                style:ElevatedButton.styleFrom(
+                  backgroundColor: AppColors.lightPrimary,
+                  foregroundColor: AppColors.primary,
+                  minimumSize: Size(AppSize.s120, AppSize.s55),
+                ) ,
+                onPressed:(){},
+                child:Text(AppStrings.back)
+            ),
+            Spacer(),
+
+            ElevatedButton(
+                style:ElevatedButton.styleFrom(
+                  minimumSize: Size(AppSize.s120, AppSize.s55),
+                ),
+                onPressed:(){},
+                child:Text(AppStrings.next))
+          ],
+        ),
+      ],
+    );
   }
 }
 
