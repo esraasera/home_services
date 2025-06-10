a01668d6cbab18de694ead091adfdf69abed5ddd
esraasera <esraam9756@gmail.com>
2025-06-10 19:55:59 +0300

    user info design done

--- lib/core/values/app_values.dart
+++ lib/core/values/app_values.dart
@@ -24,7 +24,7 @@ class AppPadding {
   static const double p20 = 20.0;
   static const double p28 = 28.0;
   static const double p50 = 50.0;
-  static const double p100 = 600.0;
+  static const double p100 = 100.0;
 }
 
 class AppSize {
