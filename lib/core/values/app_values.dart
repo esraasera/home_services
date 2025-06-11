bb98ce63ce3e7bf9b3a70b5c31c98b320fa0a599
esraasera <esraam9756@gmail.com>
2025-06-11 15:31:42 +0300

    num changing

--- lib/core/values/app_values.dart
+++ lib/core/values/app_values.dart
@@ -28,36 +28,36 @@ class AppPadding {
 }
 
 class AppSize {
+  static const double s0 = 0.0;
   static const double s0_005 = 0.005;
-  static const double s0_028 = 0.028;
   static const double s0_01 = 0.01;
-  static const double s0_148 =0.148;
   static const double s0_02 = 0.02;
-  static const double s0_03 =0.03;
-  static const double s0_035 =0.035;
-  static const double s0_04 =0.04;
-  static const double s0_049 =0.049;
-  static const double s0_05 =0.05;
-  static const double s0_06 =0.06;
-  static const double s0_075 =0.075;
-  static const double s0_08 =0.08;
-  static const double s0_09 =0.09;
-  static const double s0_12 =0.12;
-  static const double s0_15 =0.15;
-  static const double s0_18 =0.18;
-  static const double s0_2 =0.2;
-  static const double s0_23 =0.23;
-  static const double s0_28 =0.28;
-  static const double s0_3 =0.3;
-  static const double s0_33 =0.33;
-  static const double s0_35 =0.35;
-  static const double s0_4 =0.4;
-  static const double s0_5 =0.5;
-  static const double s0_8 =0.8;
-  static const double s0 = 0;
-  static const double s1 = 1;
+  static const double s0_028 = 0.028;
+  static const double s0_03 = 0.03;
+  static const double s0_035 = 0.035;
+  static const double s0_04 = 0.04;
+  static const double s0_049 = 0.049;
+  static const double s0_05 = 0.05;
+  static const double s0_06 = 0.06;
+  static const double s0_075 = 0.075;
+  static const double s0_08 = 0.08;
+  static const double s0_09 = 0.09;
+  static const double s0_12 = 0.12;
+  static const double s0_148 = 0.148;
+  static const double s0_15 = 0.15;
+  static const double s0_18 = 0.18;
+  static const double s0_2 = 0.2;
+  static const double s0_23 = 0.23;
+  static const double s0_28 = 0.28;
+  static const double s0_3 = 0.3;
+  static const double s0_33 = 0.33;
+  static const double s0_35 = 0.35;
+  static const double s0_4 = 0.4;
+  static const double s0_5 = 0.5;
+  static const double s0_8 = 0.8;
+  static const double s1 = 1.0;
   static const double s1_5 = 1.5;
-  static const int s2 = 2;
+  static const double s2 = 2.0;
   static const double s4 = 4.0;
   static const double s5 = 5.0;
   static const double s8 = 8.0;
@@ -78,6 +78,7 @@ class AppSize {
   static const double s120 = 120.0;
   static const double s150 = 150.0;
   static const double s200 = 200.0;
+
   static const double sAngle = 2 * 3.141592;
   static const double sDrawArc = -3.141592 / 2;
 }
