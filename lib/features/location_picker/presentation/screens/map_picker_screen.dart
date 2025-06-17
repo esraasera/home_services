59794ce235cefd25472073dfb7c91fc29bafe927
esraasera <esraam9756@gmail.com>
2025-06-17 21:45:02 +0300

    add map

--- lib/features/location_picker/presentation/screens/map_picker_screen.dart
+++ lib/features/location_picker/presentation/screens/map_picker_screen.dart
@@ -1,12 +1,38 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:home_services_app/core/theme/app_colors.dart';
 
 class MapPickerScreen extends StatelessWidget {
-  const MapPickerScreen({super.key});
+   MapPickerScreen({super.key});
+  static const CameraPosition _kGooglePlex = CameraPosition(
+    target:  LatLng(30.0444, 31.2357),
+    zoom: 13.0,
+  );
+  final Completer<GoogleMapController> _controller =
+  Completer<GoogleMapController>();
 
   @override
   Widget build(BuildContext context) {
    return Scaffold(
-
+     appBar: AppBar(
+       leading: IconButton(
+           onPressed: ()
+           {
+             Navigator.pop(context);
+             },
+           icon: Icon(
+             Icons.arrow_back_ios_new_outlined,
+             color: AppColors.white,
+           )),
+     ),
+  body: GoogleMap(
+    mapType: MapType.normal,
+    initialCameraPosition: _kGooglePlex,
+    onMapCreated: (GoogleMapController controller) {
+      _controller.complete(controller);
+    },
+  ),
    );
   }
 
--- lib/features/service_request/presentation/screens/user_info_screen.dart
+++ lib/features/service_request/presentation/screens/user_info_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:home_services_app/core/routing/app_routes.dart';
 import 'package:home_services_app/core/theme/app_colors.dart';
 import 'package:home_services_app/core/theme/styles_manager.dart';
 import 'package:home_services_app/core/utils/app_strings.dart';
@@ -73,38 +74,43 @@ class UserInfoScreen extends StatelessWidget {
               child: GestureDetector(
                 onTap: () {
                 },
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(AppSize.s20),
-                  child: Stack(
-                    children: [
-                      Image.asset(
-                        "assets/images/map_image.png",
-                        height: screenHeight * AppSize.s0_16,
-                        width: double.infinity,
-                        fit: BoxFit.cover,
-                      ),
-                      Positioned(
-                        bottom:AppSize.s8,
-                        right: AppSize.s10,
-                        child: Container(
-                          padding: const EdgeInsets.all(AppSize.s4),
-                          decoration: BoxDecoration(
-                            color: AppColors.black,
-                            borderRadius: BorderRadius.circular(AppSize.s20),
-                          ),
-                          child: Row(
-                            children: [
-                              Icon(Icons.location_on, color:AppColors.white, size:AppSize.s15),
-                               SizedBox(width: screenWidth * AppSize.s0_008),
-                              Text(
-                                 AppStrings.selectLocation,
-                                style: getMediumStyle(color: AppColors.white,fontSize: AppSize.s12),
-                              ),
-                            ],
-                          ),
+                child:GestureDetector(
+                  onTap:(){
+                    Navigator.of(context).pushNamed(Routes.mapRoute);
+                  },
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(AppSize.s20),
+                    child: Stack(
+                      children: [
+                        Image.asset(
+                          "assets/images/map_image.png",
+                          height: screenHeight * AppSize.s0_16,
+                          width: double.infinity,
+                          fit: BoxFit.cover,
                         ),
-                      )
-                    ],
+                        Positioned(
+                          bottom:AppSize.s8,
+                          right: AppSize.s10,
+                          child: Container(
+                            padding: const EdgeInsets.all(AppSize.s4),
+                            decoration: BoxDecoration(
+                              color: AppColors.black,
+                              borderRadius: BorderRadius.circular(AppSize.s20),
+                            ),
+                            child: Row(
+                              children: [
+                                Icon(Icons.location_on, color:AppColors.white, size:AppSize.s15),
+                                 SizedBox(width: screenWidth * AppSize.s0_008),
+                                Text(
+                                   AppStrings.selectLocation,
+                                  style: getMediumStyle(color: AppColors.white,fontSize: AppSize.s12),
+                                ),
+                              ],
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
                   ),
                 ),
               ),
