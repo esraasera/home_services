8482203b870c6cc5617ea45370293e386facab90
esraasera <esraam9756@gmail.com>
2025-07-01 01:11:52 +0300

    edit issues

--- lib/features/location_picker/presentation/controller/cubit/map_picker_cubit.dart
+++ lib/features/location_picker/presentation/controller/cubit/map_picker_cubit.dart
@@ -2,6 +2,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:home_services_app/core/utils/app_constants.dart';
 import 'package:home_services_app/features/location_picker/domain/usecases/map_use_case.dart';
+import 'package:permission_handler/permission_handler.dart';
 import '../states/map_picker_states.dart';
 
 class MapPickerCubit extends Cubit<MapPickerState> {
@@ -12,6 +13,21 @@ class MapPickerCubit extends Cubit<MapPickerState> {
   static MapPickerCubit get(context) => BlocProvider.of(context);
 
   LatLng _selectedLocation = AppConstants.defaultLocation;
+
+  Future<void> checkPermission() async {
+    emit(MapPickerLoading());
+    var status = await Permission.location.status;
+    if (status.isGranted) {
+      emit(MapPickerPermissionGranted());
+    } else {
+      var result = await Permission.location.request();
+      if (result.isGranted) {
+        emit(MapPickerPermissionGranted());
+      } else {
+        emit(MapPickerPermissionDenied());
+      }
+    }
+  }
 
   void updateLocation(LatLng newLocation) {
     _selectedLocation = newLocation;
